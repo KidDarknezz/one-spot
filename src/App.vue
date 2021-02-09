16397d58a5d1b82f734de42f0ebaafaad73ac85b
Alejandro Millan <darkslave@live.com>
2021-02-09 17:31:12 -0500

    bottom menu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,75 @@
-<template>
-  <q-layout view="lHh Lpr lFf">
+<template >
+  <q-layout view="lHh Lpr lFf" id="app">
     <q-page-container>
       <router-view />
+      <div
+        class="row fixed-bottom bg-grey-2 q-py-xs"
+        v-if="!$route.fullPath.includes('selected-assignature')"
+      >
+        <div class="col">
+          <div class="justify-center row">
+            <q-btn
+              flat
+              round
+              dense
+              icon="list_alt"
+              size="lg"
+              :color="
+                $route.fullPath.includes('my-assignatures') ? 'red' : 'black'
+              "
+              to="/my-assignatures"
+            />
+          </div>
+        </div>
+        <div class="col">
+          <div class="justify-center row">
+            <q-btn
+              flat
+              round
+              dense
+              icon="inventory_2"
+              size="lg"
+              :color="
+                $route.fullPath.includes('closed-assignatures')
+                  ? 'red'
+                  : 'black'
+              "
+              to="/closed-assignatures"
+            />
+          </div>
+        </div>
+        <div class="col">
+          <div class="justify-center row">
+            <q-btn
+              flat
+              round
+              dense
+              size="lg"
+              :color="$route.fullPath.includes('profile') ? 'red' : 'black'"
+              to="/profile"
+            >
+              <q-avatar size="40px">
+                <!-- <q-img
+                  v-if="userData.profileAvatar"
+                  :src="
+                    require(`@/assets/avatars/${userData.profileAvatar}.png`)
+                  "
+                /> -->
+              </q-avatar>
+            </q-btn>
+          </div>
+        </div>
+      </div>
     </q-page-container>
   </q-layout>
 </template>
 
+<style>
+#app {
+  font-family: "Poppins", sans-serif;
+}
+
+.os-font {
+  font-family: "Poppins", sans-serif;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,11 +16,21 @@
     <q-img :src="require('@/assets/hero.webp')" class="text-white text-bold">
       <span class="absolute-full text-h4 flex flex-center">One<br />Spot</span>
     </q-img>
+
+    <!-- <q-parallax
+      :src="require('@/assets/hero.webp')"
+      :height="600"
+      :speed="0.5"
+      style="width: 100%"
+    >
+      <div class="text-h4 text-bold text-white">OneSpot</div>
+    </q-parallax> -->
+
     <!-- /HERO -->
 
     <!-- MAIN CAROUSEL -->
     <div class="text-h5 q-pa-md">Destacados</div>
-    <carousel-3d style="height: 470px" :animationSpeed="200" :space="440">
+    <carousel-3d :animationSpeed="200" :space="440" :height="470">
       <slide
         v-for="(slide, i) in slides"
         :index="i"
@@ -33,6 +43,41 @@
       </slide>
     </carousel-3d>
     <!-- /MAIN CAROUSEL -->
+
+    <div class="row">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut
+      pretium erat. In non porttitor ante. Orci varius natoque penatibus et
+      magnis dis parturient montes, nascetur ridiculus mus. Quisque eu tincidunt
+      magna. Mauris ipsum nisi, facilisis feugiat libero non, tempus sodales
+      nisl. Proin mauris est, fermentum vel dui in, varius rutrum nibh. Nulla
+      vitae risus sit amet risus varius condimentum. Suspendisse pulvinar congue
+      ipsum, auctor dictum lacus. Sed elit quam, placerat vel vehicula in,
+      varius ac elit. Duis sit amet semper urna. Maecenas a dignissim mi.
+      Vivamus eget pellentesque tortor, id commodo nisl. Sed commodo ante in sem
+      cursus, vitae iaculis metus tempus. Mauris id turpis sit amet sapien
+      viverra fringilla. Vestibulum et massa non tortor luctus volutpat sit amet
+      vel urna. Proin porta pharetra neque facilisis auctor. Praesent mattis
+      aliquam risus vitae ullamcorper. Suspendisse elementum porta libero, non
+      convallis nisl mollis eget. Mauris porttitor vel purus eget aliquet.
+      Aliquam consectetur metus ac risus pretium, vel egestas sapien efficitur.
+      Donec ligula leo, ullamcorper et felis sed, convallis pretium sem. Morbi
+      mauris ligula, venenatis in iaculis quis, consequat ac risus. Nulla
+      convallis sagittis turpis, sed mattis metus tempus a. Donec tellus ipsum,
+      dignissim sed venenatis ac, suscipit vel felis. Morbi in egestas leo. Cras
+      eu quam condimentum, lacinia eros in, rhoncus felis. Praesent vulputate
+      sollicitudin elementum. Mauris ullamcorper rutrum odio non tristique.
+      Donec tristique sapien elit, et aliquam nisl rutrum a. Praesent urna
+      mauris, lobortis id sem ut, pellentesque porta sem. Etiam tristique ac
+      sapien in eleifend. Maecenas tristique risus eget efficitur malesuada.
+      Curabitur id imperdiet odio. Maecenas id efficitur nibh. Suspendisse
+      lobortis, urna at facilisis egestas, tellus urna volutpat ex, vitae
+      efficitur neque metus sit amet mi. Integer dapibus odio vel velit
+      elementum condimentum. Proin feugiat hendrerit euismod. Ut fermentum, mi
+      vel condimentum placerat, erat metus facilisis lacus, ut condimentum justo
+      libero sed nisl. Nulla sem nulla, tempor a diam et, rutrum luctus libero.
+      Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+      cubilia curae;
+    </div>
 
     <!-- SEARCH DIALOG -->
     <q-dialog
