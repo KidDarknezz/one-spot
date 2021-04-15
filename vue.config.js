8aa733b04a91035d1b6d968cf42237f399bbd9f5
Alejandro Millan <darkslave@live.com>
2021-04-15 11:53:51 -0500

    icons update

--- vue.config.js
+++ vue.config.js
@@ -12,21 +12,5 @@ module.exports = {
     themeColor: "#ffffff",
     msTileColor: "#ffffff",
     backgroundColor: "#ffffff",
-    manifestOptions: {
-      icons: [
-        {
-          src: "img/icons/maskable_icon.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "maskable",
-        },
-        {
-          src: "img/icons/icon-512.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "any",
-        },
-      ],
-    },
   },
 };
