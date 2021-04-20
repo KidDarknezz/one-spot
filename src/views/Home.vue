ae95bdefcda8cbc545979fb2001f6245a0308ec3
Alejandro Millan <darkslave@live.com>
2021-04-20 17:16:27 -0500

    remove s

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,7 +16,7 @@
         </div>
       </div>
       <div class="text-h5 text-bold q-px-lg" @click="reload()">
-        Lo más HOTs
+        Lo más HOT
       </div>
       <q-carousel
         animated
